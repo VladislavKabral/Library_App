aec79c06f91d463001ff95d1fe0798c071142883
VladislavKabral <mr.kabral@mail.ru>
2023-02-19 01:33:31 +0300

    Modify: controllers Add: views Add: configuration

--- src/main/java/by/kabral/springcourse/libraryapp/LibraryApp/controllers/BooksController.java
+++ src/main/java/by/kabral/springcourse/libraryapp/LibraryApp/controllers/BooksController.java
@@ -1,9 +1,33 @@
 package by.kabral.springcourse.libraryapp.LibraryApp.controllers;
 
+import by.kabral.springcourse.libraryapp.LibraryApp.services.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
+
+    private final BooksService booksService;
+
+    @Autowired
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
+    @GetMapping
+    public String booksHome(Model model) {
+        model.addAttribute("books", booksService.findAll());
+        return "books/books";
+    }
+
+    @GetMapping("/{id}")
+    public String bookHome(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", booksService.findById(id));
+        return "books/book";
+    }
 }
--- src/main/java/by/kabral/springcourse/libraryapp/LibraryApp/controllers/PeopleController.java
+++ src/main/java/by/kabral/springcourse/libraryapp/LibraryApp/controllers/PeopleController.java
@@ -1,9 +1,33 @@
 package by.kabral.springcourse.libraryapp.LibraryApp.controllers;
 
+import by.kabral.springcourse.libraryapp.LibraryApp.services.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping
+    public String peopleHome(Model model) {
+        model.addAttribute("people", peopleService.findAll());
+        return "people/people";
+    }
+
+    @GetMapping("/{id}")
+    public String personHome(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", peopleService.findById(id));
+        return "people/person";
+    }
 }
